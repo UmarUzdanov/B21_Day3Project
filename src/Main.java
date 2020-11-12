549f3250a5a91328abdc02fe53e61075af7576e6
UmarUzdanov <74148803+UmarUzdanov@users.noreply.github.com>
2020-11-12 11:13:03 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git last day!");
+        System.out.println("Did i play any music");
     }
 }
